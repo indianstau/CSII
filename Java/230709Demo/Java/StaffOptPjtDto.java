b953e54a71155f42056d3d14c8ca8693ee51dce3
indianstau <indianstau@yahoo.com.tw>
2023-07-10 01:48:40 +0900

    Create StaffOptPjtDto.java

---
+++ Java/230709Demo/Java/StaffOptPjtDto.java
@@ -0,0 +1,19 @@
+package bms.staff.dto;
+
+public class StaffOptPjtDto {
+	private int id;
+	private String pjt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPjt() {
+		return pjt;
+	}
+	public void setPjt(String pjt) {
+		this.pjt = pjt;
+	}
+	
+}

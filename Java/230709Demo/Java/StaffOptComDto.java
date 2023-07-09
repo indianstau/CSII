ead20c7aec845ea1b97983e387702b2714dd0838
indianstau <indianstau@yahoo.com.tw>
2023-07-10 01:49:13 +0900

    Create StaffOptComDto.java

---
+++ Java/230709Demo/Java/StaffOptComDto.java
@@ -0,0 +1,20 @@
+package bms.staff.dto;
+
+public class StaffOptComDto {
+	// company name from DB
+	private int id;
+	private String com;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCom() {
+		return com;
+	}
+	public void setCom(String com) {
+		this.com = com;
+	}
+	
+}

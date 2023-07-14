0dca2e9362c8278ca47c40b2bab6e1b24f40efdd
indianstau <indianstau@yahoo.com.tw>
2023-07-14 14:33:12 +0900

    Update StaffDao.java

--- Java/230709Demo/Java/StaffDao.java
+++ Java/230709Demo/Java/StaffDao.java
@@ -84,7 +84,67 @@ public class StaffDao extends CommonDAO{
 			disconnect();
 		}
 	}
+//  insert 才是staffdto  Search 是少數的聯集
+	public StaffInsertDto selectById(String uid){
+		try {
+			String sql = "SELECT * FROM syain_main WHERE SYAIN_ID = '" + uid + "'";
+			System.out.println(sql);
 
+			ResultSet rs = executeQuery(sql);
+			StaffInsertDto staff = new StaffInsertDto();
+			if(rs.next()){
+
+				staff.setFIRST_NAME_KANJI(rs.getString("FIRST_NAME_KANJI"));
+				staff.setLAST_NAME_KANJI(rs.getString("LAST_NAME_KANJI"));
+				staff.setFIRST_NAME_KANA(rs.getString("FIRST_NAME_KANA"));
+				staff.setLAST_NAME_KANA(rs.getString("LAST_NAME_KAN"));
+				staff.setFIRST_NAME_EIGO(rs.getString("FIRST_NAME_EIGO"));
+				staff.setLAST_NAME_EIGO(rs.getString("LAST_NAME_EIGO"));
+				staff.setSYUSSINN(rs.getString("SYUSSINN"));
+				staff.setPASSPORT_NUM(rs.getString("PASSPORT_NUM"));
+				staff.setKOJIN_NUM(rs.getString("KOJIN_NUM"));
+				staff.setZAIRYU_NUM(rs.getString("ZAIRYU_NUM"));
+				staff.setBIKOU(rs.getString("BIKOU"));
+				staff.setYUUBIN(rs.getString("YUUBIN"));
+				staff.setJYUSYO_1(rs.getString("JYUSYO_1"));
+				staff.setJYUSYO_2(rs.getString("JYUSYO_2"));
+				staff.setMOYORI_EKI(rs.getString("MOYORI_EKI"));
+				staff.setTEL(rs.getString("TEL"));
+				staff.setEMAIL(rs.getString("EMAIL"));
+				staff.setWECHAT(rs.getString("WECHAT"));
+				staff.setLINE(rs.getString("LINE"));
+				staff.setGAKKOU_NAME(rs.getString("GAKKOU_NAME"));
+				staff.setSENNMOM_NAME(rs.getString("SENNMOM_NAME"));
+				staff.setBOKOKU_JYUSYO(rs.getString("BOKOKU_JYUSYO"));
+				staff.setBOKOKU_KINNKYUU_RENNRAKU(rs.getString("BOKOKU_KINNKYUU_RENNRAKU"));
+
+				staff.setSEIBETU(rs.getInt(""));
+				staff.setKOKUSEKI(rs.getInt("KOKUSEKI"));
+				staff.setHAIGUSYA(rs.getInt("HAIGUSYA"));
+				staff.setVISA_KIKAN(rs.getInt("VISA_KIKAN"));
+				staff.setZAIRYU_SIKAKU(rs.getInt("ZAIRYU_SIKAKU"));
+				staff.setSYOZOKU_KAISYA(rs.getInt("SYOZOKU_KAISYA"));
+				staff.setSYOKUGYO_KIND(rs.getInt("SYOKUGYO_KIND"));
+				staff.setSAISYUU_GAKUREKI(rs.getInt("SAISYUU_GAKUREKI"));
+
+				staff.setTANJYOBI(rs.getDate("TANJYOBI"));
+				staff.setPASSPORT_END_DATE(rs.getDate("PASSPORT_END_DATE"));
+				staff.setVISA_END_DATE(rs.getDate("VISA_END_DATE"));
+				staff.setNYUUSYA_DATE(rs.getDate("NYUUSYA_DATE"));
+				staff.setTAISYA_DATE(rs.getDate("TAISYA_DATE"));
+				staff.setRAINITI_DATE(rs.getDate("RAINITI_DATE"));
+				staff.setSOTUGYO_DATE(rs.getDate("SOTUGYO_DATE"));
+
+			}
+			System.out.println(staff);
+			return staff;
+		}catch(Exception e) {
+			throw new IllegalStateException(e);
+		}finally {
+			disconnect();
+		}
+
+	}
 	// import 1. ArrayList 2.Staff dto 3.ut arraylist  4. rs
 	public ArrayList<StaffDto> search(String name, String com, String pjt, String jstat){
 		try {
